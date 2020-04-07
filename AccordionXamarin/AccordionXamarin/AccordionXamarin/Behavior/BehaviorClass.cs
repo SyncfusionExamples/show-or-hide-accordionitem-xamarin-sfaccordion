12a45e319d6ecdf6144dc3adfd6d77f17d0bea4a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-07 16:27:11 +0530

    XAMARIN-34202- Sample added.

---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Behavior/BehaviorClass.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.XForms.Accordion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AccordionXamarin
+{
+   public class BehaviorClass : Behavior<ContentPage>
+    {
+        Contact deletedItem = null;
+        SfAccordion accordion;
+        Button HideOrShow;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            accordion = bindable.FindByName<SfAccordion>("Accordion");
+            HideOrShow = bindable.FindByName<Button>("HideOrShow");
+            HideOrShow.Clicked += OnHideOrShow;
+        }
+        private void OnHideOrShow(object sender, EventArgs e)
+        {
+            var items = (sender as Button).BindingContext as ViewModel;
+
+            if (deletedItem == null)
+            {
+                deletedItem = items.ContactsInfo[2];
+                items.ContactsInfo.RemoveAt(2);
+            }
+            else
+            {
+                items.ContactsInfo.Insert(2, deletedItem);
+                deletedItem = null;
+            }
+        }
+    }
+}
---
+++ AccordionXamarin/AccordionXamarin/AccordionXamarin/Model/Contact.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AccordionXamarin
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private string contactName;
+        private string callTime;
+        private string contactImage;
+        private bool isExpanded;
+        private bool isVisible = true;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsExpanded
+        {
+            get { return isExpanded; }
+            set
+            {
+                isExpanded = value;
+                this.RaisedOnPropertyChanged("IsExpanded");
+            }
+        }
+
+        public bool IsVisible
+        {
+            get { return isVisible; }
+            set
+            {
+                isVisible = value;
+                this.RaisedOnPropertyChanged("IsVisible");
+            }
+        }
+
+        public string ContactName
+        {
+            get { return contactName; }
+            set
+            {
+                if (contactName != value)
+                {
+                    contactName = value;
+                    this.RaisedOnPropertyChanged("ContactName");
+                }
+            }
+        }
+
+        public string CallTime
+        {
+            get { return callTime; }
+            set
+            {
+                if (callTime != value)
+                {
+                    callTime = value;
+                    this.RaisedOnPropertyChanged("CallTime");
+                }
+            }
+        }
+
+        public string ContactImage
+        {
+            get { return this.contactImage; }
+            set
+            {
+                this.contactImage = value;
+                this.RaisedOnPropertyChanged("ContactImage");
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Contact()
+        {
+
+        }
+
+        public Contact(string Name)
+        {
+            contactName = Name;
+        }
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisedOnPropertyChanged(string _PropertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_PropertyName));
+            }
+        }
+
+        #endregion
+    }
+}
